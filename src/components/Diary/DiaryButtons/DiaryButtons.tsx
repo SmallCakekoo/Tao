f541ecfe2e1aad7307e28e560e4dab3faa652de3
pucapin <isabellaguerrerorivera@gmail.com>
2026-05-10 21:49:06 -0500

    fix: added onClick to show Recommendations button inside diary.

--- src/components/Diary/DiaryButtons/DiaryButtons.tsx
+++ src/components/Diary/DiaryButtons/DiaryButtons.tsx
@@ -2,9 +2,9 @@ import { IconCamera, IconMail, IconDeviceFloppy } from '@tabler/icons-react';
 import type { DiaryButtonsProps } from '../../../types/ComponentProps';
 import './DiaryButtons.css';
 
-export const DiaryButtons = ({ setCamera, handleSave }: DiaryButtonsProps) => (
+export const DiaryButtons = ({ setCamera, handleSave, setShowRecommendations }: DiaryButtonsProps) => (
   <div className="options">
-    <div className="blue-circle">
+    <div className="blue-circle" onClick={() => setShowRecommendations(true)}>
       <IconMail size={32} />
     </div>
     <div className="blue-circle" onClick={() => setCamera()}>
